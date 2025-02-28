9eeb7524fd007aac984204df0c4e6388db426985
G'olib <golibabdullaev8@gmail.com>
2025-02-28 16:27:20 +0500

    feat: delete register page; no need to register, since only an admin will add users.

---
+++ client/pages/dashboard.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-4xl font-bold text-white bg-black">Only authorized people are allowed.</h1>
+</template>
---
+++ client/stores/auth.js
@@ -0,0 +1,141 @@
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    user: null,
+    token: null,
+    isAuthenticated: false,
+    isLoading: false,
+    error: null,
+    baseUri: "http://localhost:5000",
+  }),
+
+  getters: {
+    userRole: (state) => state.user?.role || "guest",
+
+    isAdmin: (state) => state.user?.role === "admin",
+  },
+
+  actions: {
+    setLoading(status) {
+      this.isLoading = status;
+    },
+
+    setError(message) {
+      this.error = message;
+      console.log(message);
+    },
+
+    clearError() {
+      this.error = null;
+    },
+
+    async login(user) {
+      this.setLoading(true);
+      this.clearError();
+
+      try {
+        const { data, error } = await useFetch(`${this.baseUri}/auth/login`, {
+          method: "POST",
+          body: user,
+        });
+
+        if (error.value) {
+          throw new Error(error.value.message || "Failed to login");
+        }
+
+        this.setUser(data.value.user);
+        this.setToken(data.value.token);
+        this.isAuthenticated = true;
+
+        localStorage.setItem("token", data.value.token);
+
+        return data.value;
+      } catch (err) {
+        this.setError(err.message || "Failed to login");
+        throw err;
+      } finally {
+        this.setLoading(false);
+      }
+    },
+
+    async register(user) {
+      this.isLoading = true;
+      this.clearError();
+
+      try {
+        const { data, error } = await useFetch(
+          `${this.baseUri}/auth/register`,
+          {
+            method: "POST",
+            body: user,
+          }
+        );
+
+        if (error.value) {
+          throw new Error(error.value.message || "Failed to register");
+        }
+
+        this.setUser(data.value.user);
+        this.setToken(data.value.token);
+        this.isAuthenticated = true;
+
+        localStorage.setItem("token", data.value.token);
+
+        return data.value;
+      } catch (err) {
+        this.setError(err.message || "Failed to register");
+        throw err;
+      } finally {
+        this.setLoading(false);
+      }
+    },
+
+    logout() {
+      this.user = null;
+      this.token = null;
+      this.isAuthenticated = false;
+
+      localStorage.removeItem("token");
+
+      navigateTo(this.baseUri);
+    },
+
+    setUser(user) {
+      this.user = user;
+    },
+
+    setToken(token) {
+      this.token = token;
+    },
+
+    async initAuth() {
+      const token = localStorage.getItem("token");
+
+      if (!token) return;
+
+      this.token = token;
+      this.isAuthenticated = true;
+
+      try {
+        const { data, error } = await useFetch(`${this.baseUri}/auth/me`, {
+          headers: {
+            Authroization: `Bearer ${token}`,
+          },
+        });
+
+        if (error.value) {
+          throw new Error("Invalid token");
+        }
+
+        this.setUser(data.value);
+      } catch (err) {
+        this.logout();
+      }
+    },
+
+    getAuthHeaders() {
+      return this.token ? { Authorization: `Bearer ${this.token}` } : {};
+    },
+  },
+});
